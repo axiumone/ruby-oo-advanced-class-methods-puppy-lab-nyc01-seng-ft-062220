4057feb30ae3eb17e19ce15ddad5bb1db87d0a07
axiumone <axiumone@gmail.com>
2020-06-24 22:52:05 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+    attr_accessor = :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        @@all.each_with_index {|dog, i| puts @@all[i].name}
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def name
+        @name
+    end
+end
